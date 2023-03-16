46f67c57df75011a22f40f59fdd3f7a71814a980
sevenc-nanashi <sevenc7c@sevenc7c.com>
2023-03-16 19:06:22 +0900

    Fix: skypackを使うように

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
 export * as hono from "https://deno.land/x/hono@v3.0.5/mod.ts";
 export * as imagescript from "https://deno.land/x/imagescript@1.2.15/mod.ts";
 export { serve } from "https://deno.land/std@0.179.0/http/server.ts";
-export { default as ky } from "npm:ky@0.33.2";
+export { default as ky } from "https://cdn.skypack.dev/ky@0.33.2?dts";
